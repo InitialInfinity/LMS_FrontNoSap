28d8bd8455547569176737250538010794964747
admin <admin@DESKTOP-9FJ8EE0>
2024-10-15 10:23:08 +0530

    first commit

---
+++ src/Components/UrlData.js
@@ -0,0 +1,12 @@
+
+//const UrlData = "https://lms-g8a7d4egayfjg6g6.eastus-01.azurewebsites.net/api/";
+const UrlData = "https://localhost:44355/api/";
+// const UrlData = "https://tmsback.initialinfinity.com/api/"
+// const UrlData = "http://tmsback/api/";
+// const UrlData = "http://lmspublish:803/api/"
+//const UrlData = "http://lmsbackend.rensatubes.com/api/";
+//const UrlData = "http://lms.rensatubes.com:4000/api/"
+// const UrlData = "http://visualpublish.local/api/"
+
+export default UrlData;
+
